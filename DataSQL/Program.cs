7a9e0fb95c60f96852592ae893e8cd3b52d2418c
kreangkrai <kreangkrai.ratt@gmail.com>
2022-01-31 11:55:37 +0700

    update

--- DataSQL/Program.cs
+++ DataSQL/Program.cs
@@ -7,29 +7,33 @@ namespace DataSQL
     class Program
     {
         static void Main(string[] args)
-        {         
+        {
             string[] lines = File.ReadAllLines(@"D:\data.txt");
             string str = "";
             foreach (string line in lines)
             {
-                int count_space = line.Count(x => x == ' ');
-                for(int i=0;i<= count_space;i++)
+                int count_space = line.Count(x => x == '|');
+                for (int i = 0; i <= count_space; i++)
                 {
-                    if (i== 0)
+                    if (i == 0)
                     {
-                        str += "('" + line.Split(" ")[i] + "',";
+                        str += "('" + line.Split("|")[i].Trim() + "',";
                     }
-                    else if(i == count_space)
+                    else if (i == count_space)
                     {
-                        str += "'" + line.Split(" ")[i] + "'),";
+                        str += "'" + line.Split("|")[i].Trim() + "'),";
+                    }
+                    else if (i == 2)
+                    {
+                        str += "'" + Convert.ToDateTime(line.Split("|")[i]).ToString("yyyy-MM-dd") + "',";
                     }
                     else
                     {
-                        str += "'" + line.Split(" ")[i] + "',";
+                        str += "'" + line.Split("|")[i].Trim() + "',";
                     }
-                    
+
                 }
-                str += Environment.NewLine;               
+                str += Environment.NewLine;
             }
             File.WriteAllTextAsync(@"D:\datanew.txt", str);
         }
